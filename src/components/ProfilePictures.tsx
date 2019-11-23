df938fa8e8c4992a823c9a4476a54c9bc3419f53
Alexander Buczynsky <alexander.buczynsky@siemens.com>
2019-11-22 21:02:47 -0600

    nuked master

--- src/components/ProfilePictures.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import headShot from '../assets/head-shot.jpg';
-
-export const ProfilePicture: React.FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
-  return (
-    <img {...props} src={headShot} />
-  )
-}
--- src/views/ContactPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ContactPage: React.FC = () => {
-  return (
-    <div>
-      Contact Page Works
-    </div>
-  )
-}
