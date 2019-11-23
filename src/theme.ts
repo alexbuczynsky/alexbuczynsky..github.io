5b18851ef0568b9328bf0998e2bccac7fe91db9d
Alexander Buczynsky <alexander.buczynsky@siemens.com>
2019-11-22 23:23:54 -0600

    anna hates purple

--- src/theme.ts
+++ src/theme.ts
@@ -4,7 +4,7 @@ export const theme = createMuiTheme({
   palette: {
     primary: {
       // light: will be calculated from palette.primary.main,
-      main: '#ae00ff',
+      main: '#038cfc',
       // dark: will be calculated from palette.primary.main,
       // contrastText: will be calculated to contrast with palette.primary.main
     },
