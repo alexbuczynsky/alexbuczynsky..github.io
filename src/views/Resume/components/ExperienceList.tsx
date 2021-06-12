f84db4b6fe8bc34c37ba275b5202c6fad5682d6c
Alexander Buczynsky <a.buczynsky@gmail.com>
2021-06-12 10:15:10 -0500

    experience list component refactor

--- src/views/Resume/components/ExperienceList.tsx
+++ src/views/Resume/components/ExperienceList.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import List from '@material-ui/core/List'
 import { ExperienceListItem } from './ExperienceListItem'
-import { useResume } from '../../../services/data-api'
+import { IResumeExperience, useResume } from '../../../services/data-api'
 
 export const ExperienceList: React.FC = (props) => {
 
@@ -9,28 +9,32 @@ export const ExperienceList: React.FC = (props) => {
 
   return (
     <List>
-      {resume.experience.map(
-        ({
-          company,
-          jobTitle,
-          start,
-          end,
-          location,
-          responsibilities,
-          seeMoreUrl
-        }) => (
-            <ExperienceListItem
-              jobTitle={jobTitle}
-              company={company.name}
-              logoUrl={company.logo}
-              location={location}
-              startDate={start}
-              endDate={end}
-              bulletPoints={responsibilities}
-              seeMoreLink={seeMoreUrl}
-            />
-          )
-      )}
+      {resume.experience.map(experience => resumeExperienceToListItem(experience))}
     </List>
+  )
+}
+
+const resumeExperienceToListItem = (experience: IResumeExperience): JSX.Element => {
+  const {
+    company,
+    jobTitle,
+    start,
+    end,
+    location,
+    responsibilities,
+    seeMoreUrl
+  } = experience
+
+  return (
+    <ExperienceListItem
+      jobTitle={jobTitle}
+      company={company.name}
+      logoUrl={company.logo}
+      location={location}
+      startDate={start}
+      endDate={end}
+      bulletPoints={responsibilities}
+      seeMoreLink={seeMoreUrl}
+    />
   )
 }
